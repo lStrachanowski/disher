f7a873b14f2d6851c5e13ad053ea5249b011f7c1
unknown <lukasz.strachanowski@gmail.com>
2023-05-27 00:44:45 +0200

    recepie subpage created

--- disherapp/urls.py
+++ disherapp/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index )
+    path('', views.index , name="index"),
+    path('recepie/<int:id>', views.recepie, name="recepie")
 ]
--- disherapp/views.py
+++ disherapp/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "disher/index.html",{})
+
+def recepie(request , id):
+    return render(request, "disher/recepie.html")
