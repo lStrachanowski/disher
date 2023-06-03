ea45ecb91c41946141bcee56c9426f2e0e2dbea0
unknown <lukasz.strachanowski@gmail.com>
2023-06-03 15:16:00 +0200

    Spinners added to pages

---
+++ disher/static/js/script.js
@@ -0,0 +1,11 @@
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+        setTimeout(()=>{
+            document.getElementById("loader_container").style.display = "none";
+            document.getElementById("content-container").style.display = "block";
+        },1500);
+    }
+};
+xhttp.open("GET", "/", true);
+xhttp.send();
--- disherapp/views.py
+++ disherapp/views.py
@@ -2,22 +2,26 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
+def check_if_user_is_logged():
+    user_is_logged = True
+    return user_is_logged
+
 def index(request):
-    user_logged = True
-    context = {"user_status": user_logged}
+    user_status = check_if_user_is_logged()
+    context = {"user_status": user_status}
     return render(request, "disher/index.html", context)
 
 def recepie(request , id):
-    user_logged = True
-    context = {"user_status": user_logged}
+    user_status = check_if_user_is_logged()
+    context = {"user_status": user_status}
     return render(request, "disher/recepie.html", context )
 
 def dashboard(request):
-    user_logged = True
-    context = {"user_status": user_logged}
+    user_status = check_if_user_is_logged()
+    context = {"user_status": user_status}
     return render(request,"disher/dashboard.html", context)
 
 def user_profil(request):
-    user_logged = True
-    context = {"user_status": user_logged}
+    user_status = check_if_user_is_logged()
+    context = {"user_status": user_status}
     return render(request, "disher/profile.html" ,context )
